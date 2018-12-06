377c2708c8c8ee30d7a653c16b545b20fd3f665b
Jim Owens <jimcowens@gmail.com>
2018-12-06 13:45:33 -0500

    learning terminal and rearranging for better web navigability

---
+++ Bootstrap/script/script.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+console.log("hi");
+console.log("hello");
+
+var hello = "Hello";
+if (hello == "suh dude"){
+    console.log("the condition was true!");
+} else if (hello == "Hello"){
+    console.log("I totes get it.")
+}
+while (condition) {
+    
+}
+var arr=[]
+var j=0
+for (let i = 0; i < arr.length; i++) {
+    const j = arr[i];
+    
+}
