618378725de4e29fd58d6010ed1477168312a2cb
Jim Owens <jimcowens@gmail.com>
2018-12-07 15:13:29 -0500

    fixed a js bug or two

---
+++ classwork/day-6/dragon-game/faulty.js
@@ -0,0 +1,7 @@
+if (decision == "fight"){
+    dragon.innerHTML +=`
+    <img src "dragon-assets/dragon.png"/>
+`
+} else (decision == "flight"){
+    alert("You're a coward!")
+}
--- classwork/day-6/dragon-game/main.js
+++ classwork/day-6/dragon-game/main.js
@@ -1,20 +1,18 @@
 console.dir(document)
-var dragon = document.getElementById(dragon)
+var dragon = document.getElementById(dragonDiv)
 var bool = false
 while (bool != true){
-    var decision = prompt(`Look out! There appears a wild dragon in your path! 
-    What would you like to do (fight or flight)? > `)
+    var decision = prompt(`Look out! There appears a wild dragon in your path! What would you like to do (fight or flight)? > `)
     if (decision != "fight" && decision != "flight"){
         alert("Invalid input! Please choose fight or flight.")
-        alert(decision)
     } else {
         bool = true
     }
 }
 if (decision == "fight"){
-    dragon.innerHTML += `
+    dragonDiv.innerHTML +=`
     <img src="dragon-assets/dragon.png"/>
-    `
-} else (decision == "flight"){
-alert("you're a coward!!")
+`
+}else{
+    alert("You're a coward!")
 }
