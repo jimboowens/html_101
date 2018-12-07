25671e6f0c7c06fead21c450db932f571dfc33d7
Jim Owens <jimcowens@gmail.com>
2018-12-07 14:07:39 -0500

    got icon, good with command line, working on innerHTML

--- classwork/day-6/dragon-game/main.js
+++ classwork/day-6/dragon-game/main.js
@@ -4,7 +4,7 @@ var bool = false
 while (bool != true){
     var decision = prompt(`Look out! There appears a wild dragon in your path! 
     What would you like to do (fight or flight)? > `)
-    if (decision != "fight" && decision != "flight"){
+    if (decision != "fight" && decision != "flight" && decision!=null){
         alert("Invalid input! Please choose fight or flight.")
         alert(decision)
     } else {
